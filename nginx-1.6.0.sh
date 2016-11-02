54b644ce1846de8d3acca335d20dd091424d92ac
wuzijian <wuzijian@wuzijian.com>
2016-11-02 09:45:06 +0800

    nginx-install

---
+++ nginx-1.6.0.sh
@@ -0,0 +1,11 @@
+--2016-11-02 09:43:41--  http://git.jiagouyun.com/operation/operation/raw/master/nginx/nginx-1.6.0.sh
+正在解析主机 git.jiagouyun.com... 121.40.162.134
+正在连接 git.jiagouyun.com|121.40.162.134|:80... 已连接。
+已发出 HTTP 请求，正在等待回应... 200 OK
+长度：未指定 [text/plain]
+正在保存至: “nginx-1.6.0.sh.1”
+
+     0K ......                                                  612M=0s
+
+2016-11-02 09:43:41 (612 MB/s) - “nginx-1.6.0.sh.1” 已保存 [6675]
+
